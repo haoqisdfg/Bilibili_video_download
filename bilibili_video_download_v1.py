a62655c3d6c9d17a0a0273cfb928592493ed635c
haoqisdfg <90945249+haoqisdfg@users.noreply.github.com>
2021-09-30 21:54:48 +0800

    Update bilibili_video_download_v1.py

--- bilibili_video_download_v1.py
+++ bilibili_video_download_v1.py
@@ -25,7 +25,7 @@ def get_play_list(start_url, cid, quality):
     appkey, sec = ''.join([chr(ord(i) + 2) for i in entropy[::-1]]).split(':')
     params = 'appkey=%s&cid=%s&otype=json&qn=%s&quality=%s&type=' % (appkey, cid, quality, quality)
     chksum = hashlib.md5(bytes(params + sec, 'utf8')).hexdigest()
-    url_api = 'https://interface.bilibili.com/v2/playurl?%s&sign=%s' % (params, chksum)
+    url_api = 'https://www.bilibili.com/video/BV1hU4y1c7iG?spm_id_from=333.851.b_7265636f6d6d656e64.1' % (params, chksum)
     headers = {
         'Referer': start_url,  # 注意加上referer
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
